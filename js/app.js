a3a9fd0c3a1dd2824750f8b3c56d964bdf67ac25
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-10-06 18:38:37 +0200

    added-comments

--- js/app.js
+++ js/app.js
@@ -5,27 +5,29 @@
 const gridDOMElement = document.querySelector('.grid');
 console.log(gridDOMElement);
 
+
 for (let i = 1; i <= 100; i++) {
 
-    //  - creare multipli di 3     
-    //  - creare multipli di 5
     //  - e per i multipli di 3 e 5 creare una regola
+    let fizzbuzz 
 
     if (i % 15 === 0) {
         console.log('fizzbuzz');
 
+    //  - creare multipli di 5
+
     } else if (i % 5 === 0) {
         console.log('buzz');
 
+    //  - creare multipli di 3     
+    
     } else if (i % 3 === 0) {
         console.log('fizz');
         
     } else {
         console.log(i)
     }
-    
-    
-     
+
     const boxDOMElement = document.createElement('div');
     boxDOMElement.classList.add('box');
     boxDOMElement.innerHTML = i;
