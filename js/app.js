e0bde68d8433245f039300b7afbb6501d9ae04c7
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-10-06 18:03:31 +0200

    dom-element

--- js/app.js
+++ js/app.js
@@ -2,17 +2,39 @@
 
 // - creare funzione per i numeri da 1 - 100
 
+const gridDOMElement = document.querySelector('.grid');
+console.log(gridDOMElement);
+
 for (let i = 1; i <= 100; i++) {
-    n = i;
-    //  - creare multipli di 3
-    t = i % 3;
+
+    //  - creare multipli di 3     
     //  - creare multipli di 5
-    c = i % 5;
     //  - e per i multipli di 3 e 5 creare una regola
-    f = (i % 15);
-    console.log('f ' + f + ' c ' + c + ' t ' + t +  ' n ' + n)
+
+    if (i % 15 === 0) {
+        console.log('fizzbuzz');
+
+    } else if (i % 5 === 0) {
+        console.log('buzz');
+
+    } else if (i % 3 === 0) {
+        console.log('fizz');
+        
+    } else {
+        console.log(i)
+    }
+    
+    
+     
+    const boxDOMElement = document.createElement('div');
+    boxDOMElement.classList.add('box');
+    boxDOMElement.innerHTML = i;
+    gridDOMElement.append(boxDOMElement);
 }
 
 
 
 
+    // const boxString = '<div class="box">' + i + '</div>' 
+    // console.log(boxString)
+    // gridDOMElement.innerHTML += boxString;
