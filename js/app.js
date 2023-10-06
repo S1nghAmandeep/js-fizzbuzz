e0c283ef99dc78e10ef5733458e066b47a8fbcbf
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-10-06 15:22:52 +0200

    made-function-of-1/100-*-3/*-5/*-3/5

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// # FizzBuzz
+
+// - creare funzione per i numeri da 1 - 100
+
+for (let i = 1; i <= 100; i++) {
+    n = i;
+    //  - creare multipli di 3
+    t = i % 3;
+    //  - creare multipli di 5
+    c = i % 5;
+    //  - e per i multipli di 3 e 5 creare una regola
+    f = (i % 15);
+    console.log('f ' + f + ' c ' + c + ' t ' + t +  ' n ' + n)
+}
+
+
+
+
